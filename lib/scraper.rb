1ed28d38b9df92199c065989bc38d02409820ac5
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-22 10:48:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,8 +5,6 @@ html = open("https://flatironschool.com/")
 
 Nokogiri::HTML(html)
 
-doc = Nokogiri::HTML(html)
-
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
 doc.css(".headline-26OIBN")
